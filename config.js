3e4eb0d87544fef25625ab8ca4baa921d9f3f5d2
LORD PREMO <cypherx599@gmail.com>
2026-02-26 03:53:58 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  ownerName: "BROKEN LORD DEV",
+  ownerNumber: ["255773001107"], // OWNER HALISI
+  botName: "BROKEN LORD MD",
+  footer: "Powered by BROKEN LORD",
+};
